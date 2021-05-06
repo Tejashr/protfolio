25bd8506bfa94184f680b85c8f3945e038ac4794
Tejashr <tejasteju11@gmail.com>
2021-05-06 22:35:41 +0530

    about

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Nav from './nav';
+import About from './about';
 
 function App() {
   return (
     <>
-    <h1>Tejas protfolio</h1>
+     <Router>
+        <Nav></Nav>
+        <div id="colorlib-main">
+       <About></About>
+        </div>
+      </Router>
     </>
   );
 }
---
+++ src/nav.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+
+
+function Nav() {
+
+    
+    return (
+        <>
+             <div>
+        <div>
+          <nav href="#navbar" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar"><i /></nav>
+          <aside id="colorlib-aside" className="border js-fullheight">
+            <div className="text-center">
+              <div className="author-img" style={{backgroundImage: 'url(images/IMG_1188.JPG)'}} />
+              <h1 id="colorlib-logo"><a href="index.html">Tejas HR</a></h1>
+              <span className="email"><i className="icon-mail"></i>tejasteju11@gmail.com</span>
+            </div>
+            <nav id="colorlib-main-menu" role="navigation" className="navbar">
+              <div id="navbar" className="collapse">
+                <ul>
+                  <li className="active"><a href="#home" data-nav-section="home">Introduction</a></li>
+                  <li><a href="#about" data-nav-section="about">About</a></li>
+                  {/*<li><a href="#" data-nav-section="projects">Projects</a></li>
+                  <li><a href="#" data-nav-section="blog">Blog</a></li>*/}
+                  <li><a href="#timeline" data-nav-section="timeline">Timeline</a></li>
+                </ul>
+              </div>
+            </nav>
+            <nav id="colorlib-main-menu">
+              <ul>
+             
+                <li><a href="https://www.linkedin.com/in/tejas-hr-700743128/" target="_blank" rel="noopener noreferrer"><i className="icon-linkedin2" /></a></li>
+                <li><a href="https://github.com/Tejashr" target="_blank" rel="noopener noreferrer"><i className="icon-github"></i></a></li>
+               
+              </ul>
+            </nav>
+            <div className="colorlib-footer">
+              
+              <p><small>
+                Something coming soon !!
+              </small></p>
+            </div>
+          </aside>
+        </div>
+      </div>
+        </>
+    )
+}
+
+export default Nav;
