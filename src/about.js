63323847f07012a8aaaa48689c4087f278db3713
Tejashr <tejasteju11@gmail.com>
2021-05-07 16:11:07 +0530

    about

--- src/about.js
+++ src/about.js
@@ -12,10 +12,10 @@ function About(){
                 <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
                 <div className="col-md-12">
                     <div className="about-desc">
-                    <span className="heading-meta">About Us</span>
+                    <span className="heading-meta">About</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am a computer grad student from DAIICT-Gandhinagar with ICT as my major.I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years (apart from the headphone-jack part!!)</p>
-                    <p>I have started reflecting my ideas and thougths through the medium of words recently so spelling and grammer mistaks are very often.You can write me back if you spot any and don't want to live anymore :P </p>
+                    <h4>I am a computer grad student from ATME college of engg-Mysuru with CSE as my major.</h4>
+                   <h4>I love exploring new technologies and often amazed by the progress we as a human species have mad so far in the recent years </h4>
                     </div>
                 </div>
                 </div>
@@ -38,8 +38,8 @@ function About(){
                     <i className="icon-bulb" />
                 </span>
                 <div className="desc">
-                    <h3>Web Development </h3>
-                    <p>I have experience building websites and chrome extentions using JavaScript,React,HTML,CSS</p>
+                    <h3>Full Stack Web Development </h3>
+                    <p>I have experience building websites using JavaScript,ReactJS,Express,MongoDB,NodeJS,HTML,CSS</p>
                 </div>
                 </div>
             </div>
@@ -54,7 +54,7 @@ function About(){
                 </div>
                 </div>
             </div>
-            <div className="col-md-4 text-center animate-box">
+            {/* <div className="col-md-4 text-center animate-box">
                 <div className="services color-5">
                 <span className="icon">
                     <i className="icon-data" />
@@ -64,7 +64,7 @@ function About(){
                     <p>I am pursuing my internship with DevOps team at Juniper and working with tools like Jenkins, Docker, K8s</p>
                 </div>
                 </div>
-            </div>
+            </div> */}
         
             </div>
         </div>
