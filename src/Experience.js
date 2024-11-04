9c1fd791457e3c8c2dc5aab9f91e39610b88f876
Personal <personal@ip-192-168-1-80.ap-south-1.compute.internal>
2024-11-04 21:24:29 +0530

    added experince

--- src/Experience.js
+++ src/Experience.js
@@ -1,68 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import data from "./data.json";
 
 function Experience() {
-    return (
-        <>
-            <div>
-                <section className="colorlib-experience" data-section="timeline">
-                    <div className="colorlib-narrow-content">
-                        <div className="row">
-                            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
-                                <span className="heading-meta">highlights</span>
-                                <h2 className="colorlib-heading animate-box">Experience</h2>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-md-12">
-                                <div className="timeline-centered">
-                                    <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-                                        <div className="timeline-entry-inner">
-                                            <div className="timeline-icon color-1">
-                                                <i className="icon-pen2" />
-                                            </div>
-                                            <div className="timeline-label">
-                                                <h2>Full Stack Web Developer at Taskmo <span>2021 Dec - Present</span></h2>
-                                                <p>Created dashboard for client management and internal team management,Designed dashboard using reactjs,html,css,mui and bootstrap,Delivered the given products in time or before,Coded software updates to add new features, close security holes, and resolve defects,Experience in writing apis using Node and Express for business logic</p>
-                                            </div>
-                                        </div>
-                                    </article>
-                                    <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-                                        <div className="timeline-entry-inner">
-                                            <div className="timeline-icon color-1">
-                                                <i className="icon-pen2" />
-                                            </div>
-                                            <div className="timeline-label">
-                                                <h2>Full Stack Web Developer at Uraan softskills pvt ltd <span>2021 July-2021 Nov</span></h2>
-                                                <p>Cleared the bugs according to the test cases,Handled full stack project of MERN,Helped teammates by resolving their code, and i was using Reactjs as frontend and mongodb as backend and express as server</p>
-                                            </div>
-                                        </div>
-                                    </article>
-                                    <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
-                                        <div className="timeline-entry-inner">
-                                            <div className="timeline-icon color-3">
-                                                <i className="icon-pen2" />
-                                            </div>
-                                            <div className="timeline-label">
-                                                <h2>Full Stack Web development course at GUVI <span>2021 Feb-2021 June</span></h2>
-                                                <p>I joined GUVI as a Full stack web development-student. My major part of the work has been into the field of creating sustainable and flexible Full Stack Websites using Reactjs as frontend and mongodb as backend and express as server.and some basic knowledge of AWS and also some Basics of DS and Algorithms.</p>
-                                            </div>
-                                        </div>
-                                    </article>
-                                    <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
-                                        <div className="timeline-entry-inner">
-                                            <div className="timeline-icon color-none">
-                                            </div>
-                                        </div>
-                                    </article>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+  const [experience, setExperience] = useState(data.experience);
+  return (
+    <>
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div
+                className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box"
+                data-animate-effect="fadeInLeft"
+              >
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Experience</h2>
+              </div>
             </div>
-        </>
-    )
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  {experience
+                    .sort((a, b) => b.id - a.id)
+                    .map((item, index) => {
+                      return (
+                        <article
+                          className="timeline-entry animate-box"
+                          data-animate-effect="fadeInLeft"
+                          key={index}
+                        >
+                          <div className="timeline-entry-inner">
+                            <div className={`timeline-icon color-${item.id}`}>
+                              <i className="icon-pen2" />
+                            </div>
+                            <div className="timeline-label">
+                              <h2>
+                                {item.heading} <span>{item.duration}</span>
+                              </h2>
+                              <p>{item.description}</p>
+                            </div>
+                          </div>
+                        </article>
+                      );
+                    })}
+                  <article
+                    className="timeline-entry begin animate-box"
+                    data-animate-effect="fadeInBottom"
+                  >
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none"></div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default Experience;
-
